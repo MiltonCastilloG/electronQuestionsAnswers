175bcfe9b59576b9d163e7bd478c3969d01211d6
Milton <milton.castillo@alumnos.uneatlantico.es>
2019-01-17 19:29:04 +0100

    second commit

--- js/questionAnswers.js
+++ js/questionAnswers.js
@@ -13,22 +13,33 @@ questionsAnswers = [
     }
 ]
 
-
-
-questionsAnswers.forEach( (element, i) => {
-    questionN = i +1;
-    $("#secondContainer").append("<div>"
-        + "<a id='question"+i+"'>"+questionN+". "+element.question+"</a><br>"
-        + "<input type='text' class='answers' id='answer"+i+"' attr-number='"+i+"'>"
-        + "</div><br>"
-    )
+$("#addQuestion").on('click',function(e) {
+    questionsAnswers.push({
+        question: $("#newQuestion").val(),
+        answer:  $("#newAnswer").val()
+    });
+    $("#newQuestion").val("");
+    $("#newAnswer").val("");
 });
 
-$(".answers").on('keypress',function(e) {
-    if(e.which == 13) {
-        console.log($(this).val());
-        if($(this).val()== questionsAnswers[$(this).attr("attr-number")].answer)
-            console.log("Respuesta Correcta")
-    }
+$("#goToGame").on('click',function(e) {
+    $("#secondContainer").empty();
+    questionsAnswers.forEach( (element, i) => {
+        questionN = i +1;
+        $("#secondContainer").append("<div>"
+            + "<a id='question"+i+"'>"+questionN+". "+element.question+"</a><br>"
+            + "<input type='text' class='answers' id='answer"+i+"' attr-number='"+i+"'>"
+            + "</div><br>"
+        )
+    });
+    $(".answers").on('keypress',function(e) {
+        if(e.which == 13) {
+            if($(this).val()== questionsAnswers[$(this).attr("attr-number")].answer)
+                console.log("Respuesta Correcta")
+                $(this).prop("disabled", true );
+        }
+    });
+    $("#firstContainer").css("display", "none");
+    $("#secondContainer").css("display", "block");
 });
 
